fe323b50ba3dda6d8bbe43d30299bab14deb6af0
dawidb1 <dawid.baranski.96@gmail.com>
2020-04-28 21:08:36 +0200

    dockerize mock-segmentation api

--- segmentationapi/mock-segmentation-api.py
+++ segmentationapi/mock-segmentation-api.py
@@ -1,18 +1,17 @@
 import flask
-
+from flask import jsonify, Response
 
 app = flask.Flask(__name__)
 
 
 @app.route("/predict/<studyId>", methods=["GET"])
 def predict(studyId):
-    return {
-        "instanceId": "8163f709-7a92e4ec-ac21e7df-b8f77b7f-83fbe251",
-        "status": 200
-    }
+    instanceId = "8163f709-7a92e4ec-ac21e7df-b8f77b7f-83fbe251"
+    response = jsonify(instanceId=instanceId)
+    response.status_code = 200
+    return response
 
 
 if __name__ == "__main__":
     print(("* Flask starting server *"))
-    # app.run(host='0.0.0.0')
-    app.run()  # - use when working locally
+    app.run(host='0.0.0.0')
