9bc96769d5b3108e2ac2eca6255e32cdd854e140
dawidb1 <dawid.baranski.96@gmail.com>
2020-04-28 22:05:10 +0200

    webapp - segmentation integration by proxy

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -5,6 +5,10 @@ module.exports = {
         target: 'http://nginx:80',
         changeOrigin: true
       },
+      '^/predict': {
+        target: 'http://segmentation:5000',
+        changeOrigin: true
+      },
     },
   },
 };
