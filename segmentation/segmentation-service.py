c79126017cbe342b9d1cf537288b173ac249f071
dawidb1 <dawid.baranski.96@gmail.com>
2020-04-29 16:28:44 +0200

    add api-service layer

---
+++ segmentation/segmentation-service.py
@@ -0,0 +1,6 @@
+# studyID as parameter
+# get 4 modalities by studyID from orthanc
+# save on disc in folder as niigz - example flair.nii.gz
+# call my_predict from my_predict_data
+# send returned dicom to orthanc
+# return instanceID of dicom
