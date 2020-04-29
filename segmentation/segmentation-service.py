3754475fad55c7f7199e30a9b26ae098ebe1654a
dawidb1 <dawid.baranski.96@gmail.com>
2020-04-29 21:01:01 +0200

    make prediction from segmentation service

--- segmentation/segmentation-service.py
+++ segmentation/segmentation-service.py
@@ -1,20 +1,25 @@
 # studyID as parameter
-# get 4 modalities by studyID from orthanc
+# get 4 modalities by studyID from orthanc OK
 
-# save on disc in folder as niigz - example flair.nii.gz
-# call my_predict from my_predict_data
+# save on disc in folder as dicom OK
+# call my_predict from my_predict_data OK
 # send returned dicom to orthanc
 # return instanceID of dicom
 
 from orthanc_service import OrthancService
 import SimpleITK as sitk
 import os
+from Unet3DCNN.brats.my_predict_data import my_predict
 
 
 class SegmentationService:
 
     def __init__(self):
         self.orthancService = OrthancService()
+        self.predictionImagesDir = os.path.abspath("to_predict_data")
+
+    def createPrediction(self):
+        my_predict(self.predictionImagesDir, self.predictionImagesDir)
 
     def getAllModalities(self, studyId):
         modalities = ["t1", "t1ce", "flair", "t2"]
@@ -30,8 +35,7 @@ class SegmentationService:
         return self.orthancService.getInstanceById(instanceId)
 
     def saveImage(self, content, filename, format=".dcm"):
-        path = os.path.abspath(os.path.join(
-            "to_predict_data", filename + format))
+        path = os.path.join(self.predictionImagesDir, filename + format)
         with open(path, 'wb') as f:
             f.write(content)
 
@@ -41,3 +45,4 @@ if __name__ == "__main__":
     segmentationService = SegmentationService()
     studyId = "751f0eaf-29aa-4e9c-bff5-da20e9205737"
     segmentationService.getAllModalities(studyId)
+    segmentationService.createPrediction()
