b4b41e01985fb3c76793f4cd778734e06f4f9a38
dawidb1 <dawid.baranski.96@gmail.com>
2020-04-30 16:29:00 +0200

    clean up

--- segmentation/segmentation_api.py
+++ segmentation/segmentation_api.py
@@ -3,6 +3,7 @@ from flask import jsonify, Response
 from flask_cors import CORS
 from segmentation_service import SegmentationService
 from segmentation_service import OrthancService
+from segmentation_service import ApiService
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -10,12 +11,7 @@ CORS(app)
 
 @app.route("/predict/<studyId>", methods=["GET"])
 def predict(studyId):
-    # localOrthancService = OrthancService("http://localhost/orthanc")
-    dockerOrthancService = OrthancService("http://orthanc:8042")
-
-    segmentationService = SegmentationService(dockerOrthancService)
     predictionId = segmentationService.makePrediction(studyId)
-
     response = jsonify(instanceId=predictionId)
     response.status_code = 200
     return response
@@ -23,4 +19,7 @@ def predict(studyId):
 
 if __name__ == "__main__":
     print(("* Flask starting server *"))
+    apiService = ApiService('demo', 'demo')
+    dockerOrthancService = OrthancService("http://orthanc:8042", apiService)
+    segmentationService = SegmentationService(dockerOrthancService)
     app.run(host='0.0.0.0', threaded=False)
